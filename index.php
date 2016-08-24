9abb690b8183b99c3f406fcca08157c1f6ae711a
websupporter <webmaster@websupporter.net>
2016-08-24 08:42:56 +0300

    adding license properly

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
 	/**
-	 * Font Customizer Library
+	 * Font Manager
 	 * @version 1.0
 	 * @author regiohelden
 	 *
-	 * @licence: GPL
+	 * @License: GPL2 
+	 * Font Manager is free software: you can redistribute it and/or modify
+	 * it under the terms of the GNU General Public License as published by
+	 * the Free Software Foundation, either version 2 of the License, or
+	 * any later version.
+	 *
+	 * Font Manager is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	 * GNU General Public License for more details.
+	 *
+	 * You should have received a copy of the GNU General Public License
+	 * along with Font Manager. If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
+
 	 **/
 
 	if ( ! defined( 'FCL_SLUG' ) ) {
